57a0de84139506198c9383b734fe2f0cd98e7381
Levi Zitting <me@levizitting.com>
2026-08-08 15:59:45 -0500

    feat: add staging media CDN records (#1)

--- src/tf/modules/sgf.dev/main.tf
+++ src/tf/modules/sgf.dev/main.tf
@@ -173,6 +173,13 @@ locals {
       proxied = false
       ttl     = 1
     }
+    media_staging = {
+      name    = "media-staging"
+      type    = "CNAME"
+      content = "staging.sgf.dev"
+      proxied = true
+      ttl     = 1
+    }
     newsletter = {
       name    = "newsletter"
       type    = "CNAME"
@@ -312,4 +319,32 @@ resource "cloudflare_dns_record" "core" {
   comment = var.comment
   proxied = each.value.proxied
   ttl     = each.value.ttl
+}
+
+resource "cloudflare_ruleset" "cache" {
+  zone_id = var.zone_id
+  name    = "Zone cache rules"
+  kind    = "zone"
+  phase   = "http_request_cache_settings"
+
+  rules = [{
+    ref         = "cache_media_staging"
+    description = "Cache staging media for one year at the edge"
+    expression  = "http.host eq \"media-staging.sgf.dev\""
+    action      = "set_cache_settings"
+    enabled     = true
+
+    action_parameters = {
+      cache = true
+
+      edge_ttl = {
+        mode    = "override_origin"
+        default = 31536000
+      }
+
+      browser_ttl = {
+        mode = "respect_origin"
+      }
+    }
+  }]
 }
