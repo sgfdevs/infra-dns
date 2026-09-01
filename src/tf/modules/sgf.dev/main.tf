1ea0e0a87af1ec70d338a380e1329370fc283a3b
Levi Zitting <me@levizitting.com>
2026-08-31 22:20:22 -0500

    feat: enable Cloudflare gRPC

--- src/tf/modules/sgf.dev/main.tf
+++ src/tf/modules/sgf.dev/main.tf
@@ -341,3 +341,9 @@ resource "cloudflare_dns_record" "core" {
   proxied = each.value.proxied
   ttl     = each.value.ttl
 }
+
+resource "cloudflare_zone_setting" "grpc" {
+  zone_id    = var.zone_id
+  setting_id = "grpc"
+  value      = "on"
+}
