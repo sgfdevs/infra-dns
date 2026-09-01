d1cbe0576881f6a0c1ce6e9ba5334a1bd1b03a49
Levi Zitting <me@levizitting.com>
2026-08-31 22:22:07 -0500

    feat: enable Cloudflare WebSockets

--- src/tf/modules/sgf.dev/main.tf
+++ src/tf/modules/sgf.dev/main.tf
@@ -347,3 +347,9 @@ resource "cloudflare_zone_setting" "grpc" {
   setting_id = "grpc"
   value      = "on"
 }
+
+resource "cloudflare_zone_setting" "websockets" {
+  zone_id    = var.zone_id
+  setting_id = "websockets"
+  value      = "on"
+}
